8d1524861856730de9563e676625c4a73e598941
Sushruta Bera <sushrutabera1502@gmail.com>
2026-08-26 21:11:47 +0530

    Time: 7 ms (72.8%), Space: 49.9 MB (25.52%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> str = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for(int i = 0; i < strs.length; i++) {
+            String t = getSignature(strs[i]);
+            if(!map.containsKey(t)) {
+                map.put(t, new ArrayList<>());   
+                map.get(t).add(strs[i]);
+            }
+            else
+                map.get(t).add(strs[i]);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    static String getSignature(String s) {
+        char cs[] = s.toCharArray();
+        Arrays.sort(cs);
+        return new String(cs);
+    }
+}

0d430c7e4c7556b97076026a074ebfca63e54fb8
Sushruta Bera <sushrutabera1502@gmail.com>
2026-08-27 15:17:18 +0530

    Time: 33 ms (0.7%), Space: 44.3 MB (8.39%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -2,7 +2,6 @@ class Solution {
     public int minAddToMakeValid(String s) {
         int c = 0, m = 0;
         for(char ch : s.toCharArray()) {
-            // char ch = s.charAt(i);
             if(ch == '(') c++; // no of open bracket
             else if(ch == ')' && c > 0) c--; // no of open brackers that met an end bracket
             else if(ch == ')' && c <= 0) m++; // no of closed brakcet which had no open bracket

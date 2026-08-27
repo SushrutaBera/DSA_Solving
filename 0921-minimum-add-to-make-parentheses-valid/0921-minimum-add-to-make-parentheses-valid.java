59a181c4e7726110ef6e1e0615dafa45aa0be914
Sushruta Bera <sushrutabera1502@gmail.com>
2026-08-27 15:17:35 +0530

    Time: 0 ms (100%), Space: 43 MB (41.17%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -5,7 +5,6 @@ class Solution {
             if(ch == '(') c++; // no of open bracket
             else if(ch == ')' && c > 0) c--; // no of open brackers that met an end bracket
             else if(ch == ')' && c <= 0) m++; // no of closed brakcet which had no open bracket
-            System.out.println(c + " " + m);
         }
 
         return Math.abs(c + m); // total
